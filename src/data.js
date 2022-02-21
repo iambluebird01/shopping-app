a57ce2fefcc5ccc965d9420639917d4cf1192345
BlueBird <kirk.maningat@gmail.com>
2022-02-21 21:21:20 +0900

    created slider

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://img.rarible.com/prod/image/upload/t_image_preview/prod-itemImages/0xdd991ec0a7cd7c7334e3e4553aa5e4fb850168d9:10139/c7e3dcbe',
+    title: 'McDi NFT',
+    desc: "DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW NFT.",
+  },
+  {
+    id: 2,
+    img: 'https://img.rarible.com/prod/image/upload/t_image_preview/prod-itemImages/0xdd991ec0a7cd7c7334e3e4553aa5e4fb850168d9:10146/4d52fbeb',
+    title: 'HULKMANIA NFT',
+    desc: "DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW NFT.",
+  },
+  {
+    id: 3,
+    img: 'https://img.rarible.com/prod/image/upload/t_image_preview/prod-itemImages/0xdd991ec0a7cd7c7334e3e4553aa5e4fb850168d9:10156/ba58d362',
+    title: 'CHUX NFT',
+    desc: "DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW NFT",
+  },
+];
