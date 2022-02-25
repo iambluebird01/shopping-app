8f2ba4653c63747b5701e80dd4e71e76178db483
BlueBird <kirk.maningat@gmail.com>
2022-02-25 17:21:37 +0900

    created Shopping Cart Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Product from './pages/Product';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
 
 const App = () => {
-  return <ProductList />;
+  return <Cart />;
 };
 
 export default App;
