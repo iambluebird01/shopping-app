991aadd72ed1d12ff3f73af2d17eacdf36590958
BlueBird <kirk.maningat@gmail.com>
2022-02-26 14:31:53 +0900

    added responsive for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import { Search, ShoppingCartOutlined } from '@mui/icons-material';
 import Badge from '@mui/material/Badge';
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: '50px' })}
 `;
 
 const Wrapper = styled.div`
-  padding: 10px;
+  padding: 10px 20px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: '10px 0px' })}
 `;
 
 const Left = styled.div`
@@ -23,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: 'none' })}
 `;
 
 const SearchContainer = styled.div`
@@ -35,6 +39,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: '50px' })}
 `;
 
 const Center = styled.div`
@@ -44,19 +49,21 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: '24px' })}
 `;
-
 const Right = styled.div`
   flex: 1;
   display: flex;
-  align items: center;
+  align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: 'center' })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `;
 
 const Navbar = () => {
@@ -66,12 +73,12 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: 'gray', fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>BLUEBIRD.</Logo>
+          <Logo>LAMA.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
