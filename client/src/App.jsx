8fc01337a952aaccf32cf56ec823f554e64fb23d
BlueBird <kirk.maningat@gmail.com>
2022-02-27 16:52:41 +0900

    project setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
-import Product from './pages/Product';
+import React from 'react';
 import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
-import Cart from './pages/Cart';
 
 const App = () => {
   return <Home />;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
-import Announcement from '../components/Announcement';
-import Categories from '../components/Categories';
-import Navbar from '../components/Navbar';
-import Products from '../components/Products';
-import Slider from '../components/Slider';
-import Newsletter from '../components/Newsletter';
-import Footer from '../components/Footer';
 
 const Home = () => {
-  return (
-    <div>
-      <Announcement />
-      <Navbar />
-      <Slider />
-      <Categories />
-      <Products />
-      <Newsletter />
-      <Footer />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default Home;
