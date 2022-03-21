e0fbf50be36746ad96af849ae546f4e7752c656c
BlueBird <kirk.maningat@gmail.com>
2022-03-21 20:12:10 +0900

    fix TOKEN

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -5,6 +5,10 @@ const BASE_URL = 'http://localhost:5000/api/';
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
