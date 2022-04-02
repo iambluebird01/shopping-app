6e197b091c501277923bf00b5ad8ea33059f0d48
BlueBird <kirk.maningat@gmail.com>
2022-04-02 09:18:01 +0900

    updated version

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -8,7 +8,7 @@ import { mobile } from '../responsive';
 import StripeCheckout from 'react-stripe-checkout';
 import { useEffect, useState } from 'react';
 import { userRequest } from '../requestMethods';
-import { useHistory } from 'react-router';
+import { useNavigate } from 'react-router';
 
 const KEY = process.env.REACT_APP_STRIPE;
 
@@ -162,7 +162,7 @@ const Button = styled.button`
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken, setStripeToken] = useState(null);
-  const history = useHistory();
+  const history = useNavigate();
 
   const onToken = (token) => {
     setStripeToken(token);
@@ -182,7 +182,7 @@ const Cart = () => {
       } catch {}
     };
     stripeToken && makeRequest();
-  }, [stripeToken, cart.total, history]);
+  }, [stripeToken, cart.total, history, cart]);
   return (
     <Container>
       <Navbar />
